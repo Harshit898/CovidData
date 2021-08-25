43f2ce164ea4672e88fb7becc4cb358ffa3aaf90
Harshit898 <60221014+Harshit898@users.noreply.github.com>
2021-08-25 11:24:28 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const axios = require('axios');
 const cheerio = require('cheerio');
 app.use(express.static(__dirname + '/public'));
